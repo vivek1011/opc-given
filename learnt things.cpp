e0fa1139d72555909a0b6eb495d9d738f7652d53
Vivek Bansal <bansalvivek79@gmail.com>
2015-08-23 10:59:53 +0530

    Update learnt things.cpp

--- learnt things.cpp
+++ learnt things.cpp
@@ -1,6 +1,7 @@
 Calculate 
 power(a , nCr) % p ;
 input - n , r, a , p ;
-for power nCr compute nCr%(p-1) ; 
+for power nCr compute nCr%etf(p) ; 
+for prime etf(p) = p-1 ; 
 and then using power function calculate power(a , nCr%(p-1))%p ;
 check: if(a%p==0) ans= 0 ;
